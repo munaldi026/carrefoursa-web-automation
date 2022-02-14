8fdeea11290fd5b32e15999490e57afe6bd10ba7
10120557 <gunbayefkan@gmail.com>
2022-02-14 19:54:06 +0200

    add new feature

---
+++ src/test/java/com/carrefoursa/step_definitions/PLPSoldAloneProductStepDefinitions.java
@@ -0,0 +1,72 @@
+package com.carrefoursa.step_definitions;
+
+import com.carrefoursa.pages.HomePage;
+import com.carrefoursa.pages.PLPSoldAloneProduct;
+import com.carrefoursa.pages.SearchPage;
+import com.carrefoursa.pages.SupplierProductsPage;
+import com.carrefoursa.utilities.Constants;
+import com.carrefoursa.utilities.ReusableMethods;
+import io.cucumber.java.en.Given;
+
+public class PLPSoldAloneProductStepDefinitions {
+    SearchPage searchPage=new SearchPage();
+    HomePage homePage=new HomePage();
+   PLPSoldAloneProduct plpSoldAloneProduct=new PLPSoldAloneProduct();
+
+
+    @Given("Kullanici arama yapacagi urunu girer")
+    public void kullaniciAramaYapacagiUrunuGirer() {
+       homePage.searchField.sendKeys(Constants.searchProduct);
+       ReusableMethods.waitFor(3);
+
+    }
+
+    @Given("Kullanici arama buttonuna tiklar")
+    public void kullaniciAramaTiklar() {
+        homePage.searchButton.click();
+        ReusableMethods.waitFor(3);
+
+    }
+
+    @Given("Herhangi bir urun sepete eklenir")
+    public void kullaniciSepeteEklenir() {
+        searchPage.addtoBasket.click();
+        ReusableMethods.waitFor(3);
+    }
+
+    @Given("Kullanici tek basina satilan ve ayni tedarikciye ait baska bir urun adini yazar")
+    public void tekBasinaSatilan() {
+        homePage.searchField.click();
+        homePage.searchField.sendKeys(Constants.itemSoldAlone);
+    }
+
+    @Given("Devam et popup tiklanir")
+    public void devamPopupTiklanir() {
+        plpSoldAloneProduct.goOnnButton.click();
+        ReusableMethods.waitFor(2);
+    }
+
+    @Given("Kullanici sepetime tiklar")
+    public void sepeteTiklar() {
+       homePage.myBasket.click();
+       ReusableMethods.waitFor(2);
+    }
+
+    @Given("Sepete eklenen urun sepetten cikarildigini gorulur")
+    public void urunSepetenCikarilir() {
+
+    }
+
+    @Given("Vazgec popup tiklanir")
+    public void vazgecTiklanir() {
+    plpSoldAloneProduct.giveupButton.click();
+    ReusableMethods.waitFor(2);
+    }
+
+    @Given("Urun sepeten cikarilmadigi gorulur")
+    public void urunSepettenCikarilmaz() {
+
+    }
+
+
+}
---
+++ src/test/resources/features/PLPSoldAloneProduct.feature
@@ -0,0 +1,28 @@
+Feature:
+
+  Background:
+    Given Kullanici login olmus durumdadir
+    #And Kullanici Ana Sayfadaki Sepetim butonuna hover yapar
+    #Then Sepetteki Sepeti Bosalt butonuna tiklar
+    #And Cikan Popup uzerindeki Evet butonuna tiklar
+
+
+  Scenario: TC_1901 Tek Basina Satilan Urun-Ekle
+    When Kullanici arama yapacagi urunu girer
+    And Kullanici arama buttonuna tiklar
+    And Herhangi bir urun sepete eklenir
+    And Kullanici tek basina satilan ve ayni tedarikciye ait baska bir urun adini yazar
+    And Kullanici arama buttonuna tiklar
+    And Herhangi bir urun sepete eklenir
+    And Devam et popup tiklanir
+    Then Sectigi urunun sepetten kaldirildigini kontrol eder
+
+  Scenario: TC_1902 Tek Basina Satilan Urun-Vazgec
+    When Kullanici arama yapacagi urunu girer
+    And Kullanici arama buttonuna tiklar
+    And Herhangi bir urun sepete eklenir
+    And Kullanici tek basina satilan ve ayni tedarikciye ait baska bir urun adini yazar
+    And Kullanici arama buttonuna tiklar
+    And Herhangi bir urun sepete eklenir
+    And Vazgec popup tiklanir
+    Then Urun sepeten cikarilmadigi gorulur
